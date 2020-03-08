44b0f2816679955e6fea4d6520e72edb1c74994a
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-03-08 18:33:25 +0100

    delete var and establish all variables as const

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function buttonClicked(argButtonName){
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
 
-var computerMove, playerMove, randomNumber;
+
 
         /**
          * Describe this function...
@@ -46,11 +46,11 @@ var computerMove, playerMove, randomNumber;
         }
 
 
-playerMove = argButtonName;
+const playerMove = argButtonName;
 console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
+const computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
 
