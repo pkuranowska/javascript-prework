be8f18fe5e32ac796a0333bd636a0c681ce4b141
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-03-08 20:21:29 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -57,15 +57,19 @@ displayResult(playerMove, computerMove);
 }
 
 const buttonRock = document.getElementById('button-rock');
-const buttonPaper = document.getElementById('button-paper');
-const buttonScissors = document.getElementById('button-scissors');
 
 buttonRock.addEventListener('click', function(){ 
   buttonClicked('kamień');
  });
+
+const buttonPaper = document.getElementById('button-paper');
+
 buttonPaper.addEventListener('click', function(){ 
   buttonClicked('papier'); 
 });
+
+const buttonScissors = document.getElementById('button-scissors');
+
 buttonScissors.addEventListener('click', function(){ 
   buttonClicked('nożyce'); 
 });
